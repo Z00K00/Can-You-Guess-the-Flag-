b94afc39416127417fdc16c18566f589c362e17d
kraiglarson2 <kraigalarson2@gmail.com>
2023-12-09 12:35:04 -0500

    Initial Commit

---
+++ Guess the Flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Krajg Larson on 12/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
